0ddbde487e1c814b083b79fa67a24f83f4339f70
Najma <najmathande@gmail.com>
2023-04-21 17:53:59 +0300

    done assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setCart] = useState(false)
+  const addItemToCart = () => {
+    setCart(true);
+  }
+
+
   return (
     <li className="">
       <span>{name}</span>
+
       <span className="category">{category}</span>
+
       <button className="add">Add to Cart</button>
+      
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,6 +2,26 @@ import React from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const handleFilterCategory = (e) =>{
+    setSelectedCategory(e.target.value);
+
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+
+      return item.category === selectedCategory;
+    }
+  }); 
+
+  const itemList = itemsToDisplay.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  )); 
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
